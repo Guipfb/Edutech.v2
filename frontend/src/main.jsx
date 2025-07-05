c7e401adf404098d2b90ffc3dd844781aa260a76
Guipfb <guilhermeisaias12@outlook.com>
2025-07-05 14:58:41 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";      // Seu App.jsx principal
+import Aula1 from "./Aula1";  // Aula criada
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/aulas/aula1" element={<Aula1 />} />
+        {/* <Route path="/aulas/aula2" element={<Aula2 />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
